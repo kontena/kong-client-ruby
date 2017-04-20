c6eaca9755d4f8bae48d9c8e5bcd3db39bbdea1b
Lauri Nevala <lauri.nevala@gmail.com>
2017-04-20 10:38:47 +0300

    Transform config keys to config.key format on plugin create and update (#12)

--- lib/kong/plugin.rb
+++ lib/kong/plugin.rb
@@ -5,5 +5,27 @@ module Kong
 
     ATTRIBUTE_NAMES = %w(id api_id name config enabled consumer_id).freeze
     API_END_POINT = '/plugins/'.freeze
+
+    # Create resource
+    def create
+      if attributes['config']
+        attributes['config'].each do |key, value|
+          attributes["config.#{key}"] = value
+        end
+        attributes.delete('config')
+      end
+      super
+    end
+
+    # update resource
+    def update
+      if attributes['config']
+        attributes['config'].each do |key, value|
+          attributes["config.#{key}"] = value
+        end
+        attributes.delete('config')
+      end
+      super
+    end
   end
 end
